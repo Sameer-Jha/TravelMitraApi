fa97c5af35aaa10f039f282ac9c8e49fb242608d
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-06-29 16:55:01 +0530

    minor changes

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return('''<h2>You are at the root of all cause 🙋</h2><br><hr><br>
-    <h4>Usage ⚒:</h4><br><br>
-    <b>Hotel Search</b> &emsp: https://travelmitra-api.herokuapp.com/hotel?city=<city_name> <br>
-    <b>Flight Search</b> &emsp: https://travelmitra-api.herokuapp.com/flight?src=<src_iata_code>&dest=<dest_iata_code>&class=<B or E>&date=<YYYYMMDD><br>
-    <b>Bus Search</b> &emsp: https://travelmitra-api.herokuapp.com/bus?src=<src_city_name>&dest=<dest_city_name>&date=<YYYYMMDD> <br><hr><br><br>©Sameer@TeamEdith-SIH 2020''')
+    return('''<h2>You are at the root of all cause 🙋</h2><br><hr>
+    <h4>Usage ⚒:</h4><br>
+    <b>Hotel Search</b>: https://travelmitra-api.herokuapp.com/hotel?city=(city_name) <br>
+    <b>Flight Search</b>: https://travelmitra-api.herokuapp.com/flight?src=(src_iata_code)&dest=(dest_iata_code)&class=(B or E)&date=(YYYYMMDD)<br>
+    <b>Bus Search</b>: https://travelmitra-api.herokuapp.com/bus?src=(src_city_name)&dest=(dest_city_name)&date=(YYYYMMDD) <br><hr><br><br><b>©Sameer@TeamEdith-SIH 2020</b>''')
 
 @app.route('/hotel')
 def hotel_booking():
