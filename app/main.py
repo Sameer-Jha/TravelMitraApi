04ada3969f3444d32445af84d6ac6b7d5054e58f
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-06-29 13:03:08 +0530

    local development environment

---
+++ app/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+import master
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return('<h2>You are at the root of all cause 🙋</h2>')
+
+@app.route('/hotel')
+def hotel_booking():
+    city=request.args.get('city')
+    return(master.hotel_search(city))
+
+@app.route('/flight')
+def flight_booking():
+    src=request.args.get('src')
+    dest=request.args.get('dest')
+    seating=request.args.get('class')
+    date=request.args.get('date')
+    return(master.flight_search(src,dest,seating,1,0,0,date))
+
+@app.route('/bus')
+def bus_booking():
+    src=request.args.get('src')
+    dest=request.args.get('dest')
+    date=request.args.get('date')
+    return(master.bus_search(src,dest,date))
+
+if __name__ == "__main__":
+    app.run()
